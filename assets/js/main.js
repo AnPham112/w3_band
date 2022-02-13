e421aad155a9ca94ea52a232c7b2c744c5c63a35
anpham112 <anpham1122000@gmail.com>
2022-02-13 23:42:58 +0700

    add javascript for slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,6 @@ mobileMenu.onclick = function () {
 const menuItems = document.querySelectorAll('#nav li a[href*="#"]');
 for (let i = 0; i < menuItems.length; i++) {
   let menuItem = menuItems[i];
-  console.log(menuItem.nextElementSibling);
   const isParentMenu = menuItem.nextElementSibling && menuItem.nextElementSibling.classList.contains('subnav');
   // click on navigation item
   menuItem.onclick = function (e) {
@@ -55,3 +54,82 @@ modalContainer.addEventListener('click', function (event) {
   event.stopPropagation();
 })
 
+
+const sliders = [
+  {
+    id: 1,
+    img: `url(../../assets/img/slider/slider1.jpg)`,
+    title: "Los Angeles",
+    desc: "We had the best time playing at Venice Beach!"
+  },
+  {
+    id: 2,
+    img: `url(../../assets/img/slider/slider2.jpg)`,
+    title: "New York",
+    desc: "This is New York"
+  },
+  {
+    id: 3,
+    img: `url(../../assets/img/slider/slider3.jpg)`,
+    title: "London",
+    desc: "This is London"
+  },
+];
+
+const sliderImg = document.getElementById('slider');
+const title = document.querySelector('.text-heading');
+const description = document.querySelector('.text-description');
+const dotItems = document.querySelectorAll('.slider-dot-item');
+const prevBtn = document.querySelector('.slider-prev');
+const nextBtn = document.querySelector('.slider-next');
+
+//set current item
+let currentItem = 0;
+
+//load initial item
+window.addEventListener('DOMContentLoaded', function () {
+  showSlider();
+})
+
+function showSlider() {
+  const item = sliders[currentItem];
+  sliderImg.style.backgroundImage = item.img;
+  title.textContent = item.title;
+  description.textContent = item.desc;
+}
+
+nextBtn.addEventListener('click', function () {
+  handleChangeSlide(1)
+})
+prevBtn.addEventListener('click', function () {
+  handleChangeSlide(-1)
+})
+
+function handleChangeSlide(direction) {
+  if (direction === 1) {
+    currentItem++;
+    if (currentItem > sliders.length - 1) {
+      currentItem = 0
+    }
+    showSlider()
+  } else if (direction === -1) {
+    currentItem--;
+    if (currentItem < 0) {
+      currentItem = sliders.length - 1;
+    }
+    showSlider()
+  }
+  [...dotItems].forEach(el => el.classList.remove('active'));
+  dotItems[currentItem].classList.add('active')
+}
+
+[...dotItems].forEach((item) =>
+  item.addEventListener('click', function (e) {
+    // the rest of dotItems
+    [...dotItems].forEach(el => el.classList.remove('active'));
+    e.target.classList.add('active');
+    const slideIndex = parseInt(e.target.dataset.index);
+    currentItem = slideIndex;
+    showSlider();
+  })
+)
